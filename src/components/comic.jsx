56a9ec6c4b22fe689ceda25963a3eb1d900c15fb
kkumar-gcc <krishkumar9352@gmail.com>
2023-11-27 03:11:44 +0530

    add card for image

--- src/components/comic.jsx
+++ src/components/comic.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import * as yup from "yup";
 import {useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
-import {appendComic} from "../redux/slice/comicSlice";
+import {appendComic, setComics} from "../redux/slice/comicSlice";
+import Card from "./card";
 
 const schema = yup
     .object({
@@ -14,6 +15,9 @@ const schema = yup
 export default function Comic() {
     const dispatch = useDispatch();
     const [isGenerating, setIsGenerating] = React.useState(false);
+    const cms = useSelector(state => state.comic.comics);
+    const comics = [...cms].sort((a, b) => b.time - a.time)
+
     const {
         register,
         setError,
@@ -54,6 +58,10 @@ export default function Comic() {
         }
     })
 
+    const handleClear = () => {
+        dispatch(setComics([]))
+    }
+
     React.useEffect(() => {
         if (isSubmitSuccessful) {
             reset({inputs: ""})
@@ -61,6 +69,9 @@ export default function Comic() {
         }
     }, [clearErrors, isSubmitSuccessful, reset])
 
+    React.useEffect(() => {
+        dispatch(setComics([]))
+    }, [])
     return (
         <div className="p-2 sm:p-4 flex flex-col lg:flex-row">
             <div className={"flex-1 lg:w-1/3"}>
@@ -81,17 +92,31 @@ export default function Comic() {
                                 disabled={!isDirty || !isValid}>
                             {isGenerating ? "Generating..." : "Generate comic"}
                         </button>
+                        <button type={"button"}
+                                className={"bg-rose-600 border shadow rounded-lg px-2 py-1 border-rose-800 text-white disabled:bg-rose-300 disabled:border-rose-400 ml-2"}
+                                onClick={handleClear}
+                                disabled={isGenerating}>
+                            Delete Images
+                        </button>
                     </div>
                 </form>
             </div>
             <div className={"lg:pl-12 lg:w-3/4"}>
-                <div className={"flex flex-col border p-4 rounded-lg text-center border-gray-600 my-2"}>
-                    <h5 className={"font-medium text-rose-800 text-lg"}>there is no comic yet :(</h5>
-                    <p>please generate one by entering a context in the input box</p>
-                </div>
+                {comics.length === 0 &&
+                    <div className={"flex flex-col border p-4 rounded-lg text-center border-gray-400 my-2 shadow"}>
+                        <h5 className={"font-medium text-rose-800 text-lg"}>there is no comic yet :(</h5>
+                        <p>please generate one by entering a context in the input box</p>
+                    </div>
+                }
+                {comics.length > 0 &&
+                    <div className={"grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4"}>
+                        {comics.map((comic, index) => (
+                            <Card key={index} comic={comic}/>
+                        ))}
+                    </div>
+                }
             </div>
 
-            {/*<img src={"blob:http://localhost:3000/8f8c5d03-98bd-42ac-94b4-6b19f47f3dd6"} alt="Comic" />*/}
         </div>
     );
 }
